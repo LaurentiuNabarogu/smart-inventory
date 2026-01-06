e6ce4ad4e0505c305d563af6bf17a1fa73e9ffab
LaurentiuNabarogu <laurentiu.nabarogu@gmail.com>
2026-01-06 21:20:54 +0200

    Make the id readonly for the ItemDTO

--- src/main/java/com/spring_app/smart_inventory/dto/ItemDTO.java
+++ src/main/java/com/spring_app/smart_inventory/dto/ItemDTO.java
@@ -1,11 +1,12 @@
 package com.spring_app.smart_inventory.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
 
-public record ItemDTO(UUID id,String name,String description, UUID categoryId, String serialNumber, Instant createdAt) {
+public record ItemDTO(@JsonProperty(access = JsonProperty.Access.READ_ONLY) UUID id, String name, String description, UUID categoryId, String serialNumber, Instant createdAt) {
 
 }
