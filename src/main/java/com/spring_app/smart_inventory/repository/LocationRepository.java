82e0941af8ae5c6a9dce98507bb1d406a5d53d7b
LaurentiuNabarogu <laurentiu.nabarogu@gmail.com>
2026-01-18 16:56:25 +0200

    Fixing the import error

--- src/main/java/com/spring_app/smart_inventory/repository/LocationRepository.java
+++ src/main/java/com/spring_app/smart_inventory/repository/LocationRepository.java
@@ -1,6 +1,9 @@
 package com.spring_app.smart_inventory.repository;
 
+import com.spring_app.smart_inventory.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
 
 public interface LocationRepository extends JpaRepository<Location, UUID> {
 }
